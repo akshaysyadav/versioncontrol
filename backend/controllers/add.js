0d902ff5daa407419a4fde7fab5a79c5bc432633
Akshay Yadav <akshaysyadav2005@gmail.com>
2026-04-04 00:45:07 +0530

    :wqcommit controller

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,4 +1,17 @@
+const fs = require('fs').promises;
+const path  = require('path');
+
 async function addRepo(file) {
-    console.log("File added to staging area");
+    const repoPath = path.resolve(process.cwd(), '.hidden_repo');
+    const stagingPath = path.join(repoPath, 'staging');
+    try {
+        await fs.mkdir(stagingPath, { recursive: true });
+        const filePath = path.basename(file);
+        await fs.copyFile(file, path.join(stagingPath, filePath));
+        console.log(`File ${file} added to staging area.`);
+    } 
+    catch (err) {
+        console.error('Error adding file to staging area:', err);
+    }
 }
 module.exports = { addRepo };
