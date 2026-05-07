86ff3469726b138c77b7daee306f1f6a8f6dc472
Akshay Yadav <akshaysyadav2005@gmail.com>
2026-05-07 14:32:54 +0530

    frontend files

---
+++ frontend/src/authContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(() => {
+    return localStorage.getItem("userId");
+  });
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
